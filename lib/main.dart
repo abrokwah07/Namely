0a95f4072cf0a88a40f3c561bfa56359075eb2a8
Kwadwo Abrokwah <abrokwakwadwo85@gmail.com>
2023-03-27 23:15:49 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,26 +28,49 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('we starting this hard!!!'),
-          Text(appState.current.asLowerCase),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: BigCard(pair: pair),
+          ),
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext(); // this will call the getNext function
             },
             child: Text('Next'),
           ),
         ],
       ),
     );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    Key? key,
+    required this.pair,
+  }) : super(key: key);
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
   }
 }
